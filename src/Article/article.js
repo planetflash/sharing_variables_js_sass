8bfdeb23ac2f63628426f8947c16dc18516a02b0
Paul Thomas <paul@craftedbygc.com>
2018-03-06 20:49:10 +0000

    update article link

--- src/Article/article.js
+++ src/Article/article.js
@@ -3,7 +3,8 @@ import "./article.scss";
 const article = `<article class="article">
     <h1>Sharing variables between JS and Sass using webpack sass-loader</h1>
     <p>This is the GitHub repo and project files for the article on how to share variables between JS and Sass using webpack and sass-loader.</p>
-    <p><a href="https://medium.com/p/713f51fa7fa0/">Read the full article</a>
+    <p><a href="https://medium.com/@paul.motionimaging/sharing-variables-between-js-and-sass-using-webpack-sass-loader-713f51fa7fa0
+">Read the full article</a>
   </article>`;
 
 export default article;
